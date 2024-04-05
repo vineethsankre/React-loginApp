92e181cbed9b184a114746bca5677f3138b05b5e
vineethsankre <lsvineethsankre@gmail.com>
2024-04-05 07:03:11 +0000

    Login page created

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,28 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+import Message from '../Message'
+import Login from '../Login'
+import Logout from '../Logout'
+
+class Home extends Component {
+  state = {isLoggedIn:false}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <div className="home-container"></div>
+          <Message isLoggedIn={isLoggedIn}/>
+          {isLoggedIn ? (
+            <Logout logout ={this.onClickButton}/>
+          ) : (
+            <Login login = {this.onClickButton}/>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
